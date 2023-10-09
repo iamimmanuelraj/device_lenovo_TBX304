96584feac4b6534e4a57ce93383865de366f227f
Michael Bestas <mkbestas@lineageos.org>
2023-10-09 19:32:36 +0530

    TBX304: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: I3cd279191c7bd3582f1c1f80c588eb26cf9da933
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -2,18 +2,13 @@
 #
 # Copyright (C) 2019 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# SPDX-License-Identifier: Apache-2.0
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
 set -e
 
@@ -22,6 +17,4 @@ export DEVICE=TBX304
 export DEVICE_COMMON=tb-common
 export VENDOR=lenovo
 
-export DEVICE_BRINGUP_YEAR=2020
-
-./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
