9dccc8a0a911c19932a58b66c5d296203b543010
Immanuel Raj <iamimmanuelraj@gmail.com>
2023-10-12 17:01:15 +0530

    TBX304: Inherit Car make file

--- lineage_TBX304.mk
+++ lineage_TBX304.mk
@@ -18,6 +18,9 @@
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 1280
 
+# Inherit Car configuration
+$(call inherit-product, device/common/car/car.mk)
+
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
